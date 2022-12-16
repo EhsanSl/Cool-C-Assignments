7eff041bf59484dce741cc0e8f94241df309cb91
Ehsan <76597689+EhsanSl@users.noreply.github.com>
2022-12-16 03:40:55 -0500

    ..
    ..

---
+++ Arithmetic-On-Binaries/src/logic.h
@@ -0,0 +1,12 @@
+
+
+#ifndef DD_LAB4_LOGIC_H
+#define DD_LAB4_LOGIC_H
+
+void func_or(int a[], int b[], int result[]);
+
+void func_and(int a[], int b[], int result[]);
+
+void func_not(int a[], int result[]);
+
+#endif //DD_LAB4_LOGIC_H
